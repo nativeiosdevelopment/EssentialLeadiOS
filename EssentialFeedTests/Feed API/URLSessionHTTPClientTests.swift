53f50fccb0417f58cdc39df3827c7f5cf647708e
John <john@zelty.fr>
2022-09-13 16:49:30 +0200

    Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect our tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -29,7 +29,8 @@ class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.url, url)
             exp.fulfill()
         }
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
+        
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -38,11 +39,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
-        let sut = URLSessionHTTPClient()
-
         let exp = expectation(description: "Wait for completion")
-
-        sut.get(from: url) { result in
+        
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -111,5 +110,11 @@ class URLSessionHTTPClientTests: XCTestCase {
             client?.urlProtocolDidFinishLoading(self)
         }
         override func stopLoading() {}
+    }
+}
+
+private extension URLSessionHTTPClientTests {
+    func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
     }
 }
